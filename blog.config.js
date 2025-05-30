e9c6c664837b9586d8d94b3e99f9ebbc8a2f1767
topgzbn <likun000309@163.com>
2025-05-30 10:52:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'gzbn', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '孤枕百年', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '学无止境🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://gzbn.us.kg',
   // LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
